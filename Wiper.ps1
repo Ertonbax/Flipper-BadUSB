3612dfaa974e5a39978e173a63c4870354314fea
Ertonbax <marcus.schuier.ms@gmail.com>
2026-08-09 14:59:53 +0200

    Delete Wiper.ps1

--- Wiper.ps1
+++
@@ -1,8 +0,0 @@
-# 1. Looking for Harddrive called "USB-Silver"
-$target = Get-Volume | Where-Object { $_.FileSystemLabel -eq "USB-Silver" }
-
-if ($target) {
-    $driveLetter = $target.DriveLetter + ":"
-    Write-Host "[*] Harddrive $driveLetter detected. Wiping Harddrive..." -ForegroundColor Yellow
-    Remove-Item "$driveLetter\*" -Recurse -Force -ErrorAction SilentlyContinue
-}
